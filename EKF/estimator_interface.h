7f76d90575d92c8c11fae4dc62508552a5aa2b3f
bugobliterator <siddharthbharatpurohit@gmail.com>
2016-02-17 12:13:03 -0800

    EKF: implement state machine for optical flow fusion selection

--- EKF/estimator_interface.h
+++ EKF/estimator_interface.h
@@ -203,6 +203,7 @@ protected:
 	float _gps_origin_eph = 0.0f; // horizontal position uncertainty of the GPS origin
 	float _gps_origin_epv = 0.0f; // vertical position uncertainty of the GPS origin
 	struct map_projection_reference_s _pos_ref = {};    // Contains WGS-84 position latitude and longitude (radians)
+
 	float _gps_origin_eph = 0.0f; // horizontal position uncertainty of the GPS origin
 	float _gps_origin_epv = 0.0f; // vertical position uncertainty of the GPS origin
 
