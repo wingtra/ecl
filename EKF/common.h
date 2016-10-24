36f10c0f4b465e74882c0ad56999e868434face9
Roman <bapstroman@gmail.com>
2016-10-24 09:40:22 +0200

    ekf reset: more granular reset / timeout strategy for gps fusion
    - if both gps position and velocity measurements are rejected for 7 seconds
    do a reset
    - if only gps position measurements are rejected then wait for 14 seconds
    as we still have velocity measurements to constrain the drift in position
    - introduced ecl internal parameter for the timeout
    
    Signed-off-by: Roman <bapstroman@gmail.com>

--- EKF/common.h
+++ EKF/common.h
@@ -262,6 +262,9 @@ struct parameters {
 	float vel_Tau;	// velocity state correction time constant (1/sec)
 	float pos_Tau;	// postion state correction time constant (1/sec)
 
+	unsigned no_gps_timeout_max;	// maximum time we allow dead reckoning while both gps position and velocity measurements are being
+									// rejected
+
 	// Initialize parameter values.  Initialization must be accomplished in the constructor to allow C99 compiler compatibility.
 	parameters()
 	{
@@ -352,6 +355,8 @@ struct parameters {
 		// output complementary filter tuning time constants
 		vel_Tau = 0.5f;
 		pos_Tau = 0.25f;
+
+		no_gps_timeout_max = 7e6;	// maximum seven seconds of dead reckoning time for gps
 
 	}
 };
